1f5d55a99ce25833029477585449fca12a3d05e6
b5030816 <b5030816@my.shu.ac.uk>
2017-04-05 22:14:04 +0100

    librarian login complete

--- librarySystem/grails-app/controllers/com/lhenry/LibrarianController.groovy
+++ librarySystem/grails-app/controllers/com/lhenry/LibrarianController.groovy
@@ -4,5 +4,29 @@ class LibrarianController {
 
 def scaffold= Librarian
 
-    
+
+def login() {
+}
+def validate() {
+def user = Librarian.findByUserName(params.username)
+if (user && user.password == params.password){
+
+session.user=user
+render view:'home'
+}
+else{
+
+flash.message="invalid username and password."
+
+render view:'login'
+
+}
+
+}
+
+def logout = {
+session.user = null
+
+redirect(url:'/')
+}
 }
--- librarySystem/grails-app/controllers/com/lhenry/StudentController.groovy
+++ librarySystem/grails-app/controllers/com/lhenry/StudentController.groovy
@@ -4,5 +4,25 @@ class StudentController {
 
 def scaffold= Student
 
-    
+
+def login() {
 }
+def validate() {
+def user = Student.findByUserName(params.username)
+if (user && user.password == params.password){
+session.user=user 
+render view:'home'
+}
+else{
+flash.message="invalid username and password."
+
+render view:'login'
+
+}
+}
+def logout = {
+session.user = null
+redirect(urri:'/')
+}
+}
+
